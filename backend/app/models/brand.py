9c4f26e66a907e27fe046dcf6ca8a446f55ef409
ugur berktas <ugur_berktas@icloud.com>
2025-10-29 18:24:04 +0300

    deneme commiti

--- backend/app/models/brand.py
+++ backend/app/models/brand.py
@@ -2,4 +2,9 @@ from ..database import Base
 
 class Brand(Base):
     __tablename__ = "brands"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False, index=True)
+    email = Column(String, unique=True, nullable=False, index=True)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
     
